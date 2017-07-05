4422d939c6768ff905031db91e5584856f05f19c
Pieter-Jan Vandenberghe <pieterjan.vandenberghe@ugent.be>
2017-07-05 13:31:16 +0200

    Filter trips by arrivalStop and arrivalTime

--- data/trips.js
+++ data/trips.js
@@ -6,10 +6,22 @@ class Trips {
     let trips = JSON.parse(fs.readFileSync(path.join(__dirname, './trips.json'), {
       encoding: 'utf8'
     }));
-    return trips.filter((trip) => trip["@id"] === id);
+    return trips.filter((trip) => trip["@id"] === id)[0];
+  }
+
+  findByArrivalStopAndTime(arrivalStop, arrivalTime) {
+    let trips = JSON.parse(fs.readFileSync(path.join(__dirname, './trips.json'), {
+      encoding: 'utf8'
+    }));
+    return trips.filter((trip) => {
+      let date = new Date(trip.arrivalTime);
+      let time = date.getTime();
+      return trip.arrivalStop === arrivalStop && time === arrivalTime;
+    })[0];
   }
 }
 
-//console.log(findById("http://irail.be/connections/8885001/20170705/IC1909"));
+//console.log(new Trips().findById("http://irail.be/connections/8885001/20170705/IC1909"));
+//console.log(new Trips().findByArrivalStopAndTime("http://irail.be/stations/NMBS/008886348", 1499240640000));
 
 export default Trips;
